a2b5da81aa2e505662fa0d0a93f3edc7f3fda505
urkopineda <urkopineda@gmail.com>
2015-06-18 10:02:56 +0200

    v0.1.1 	- Añadidas clases iniciales (Objetos, Utils, etc.) 	- Añadidos métodos de conexión de MySQL a 'DataBaseUtils'. 	- Añadida librería MySQL.

---
+++ feedScores/src/data/ConfigData.java
@@ -0,0 +1,9 @@
+package data;
+
+public class ConfigData {
+	public static String dbName = null;
+	public static String dbURL = null;
+	public static String dbUser = null;
+	public static String dbPassword = null;
+	public static int dbPort = 0;
+}
