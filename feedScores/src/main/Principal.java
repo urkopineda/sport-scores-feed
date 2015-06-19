098c09565a91b1f2a3c7f61211167800efffef8e
urkopineda <urkopineda@gmail.com>
2015-06-19 11:58:57 +0200

    v0.2.3 	- Resueltos conflictos.

---
+++ feedScores/src/main/Principal.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.Date;
+
+import socket.ClientSocketConnection;
+import socket.ServerSocketConnection;
+import data.ConfigData;
+
+public class Principal {
+	public static void main(String args[]) {
+		ConfigData.deviceMode = args[0];
+		if (ConfigData.deviceMode.equals("server")) {
+			ServerSocketConnection serverMode = new ServerSocketConnection();
+			while (true) {
+				serverMode.printMessage(new Date().toString());
+			}
+		} else if (ConfigData.deviceMode.equals("client")) {
+			ClientSocketConnection clientMode = new ClientSocketConnection();
+			while (true) {
+				System.out.println(clientMode.getMessage());
+			}
+		}
+	}
+}
